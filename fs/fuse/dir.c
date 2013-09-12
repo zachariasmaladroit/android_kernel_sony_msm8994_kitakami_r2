bd23d6177ebf7c4db588fd5b749712fe6e29ec8d
Kirill A. Shutemov <kirill.shutemov@linux.intel.com>
2013-09-12 15:13:56 -0700

    truncate: drop 'oldsize' truncate_pagecache() parameter
    truncate_pagecache() doesn't care about old size since commit
    cedabed49b39 ("vfs: Fix vmtruncate() regression").  Let's drop it.
    
    Signed-off-by: Kirill A. Shutemov <kirill.shutemov@linux.intel.com>
    Cc: OGAWA Hirofumi <hirofumi@mail.parknet.co.jp>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    
    Conflicts:
    	fs/fuse/dir.c
    
    due to 'fuse: Trust kernel i_size only'
    ddea8de423a4c5cb45b48bd48a51060df1352121
    
    Signed-off-by: zachariasmaladroit <zacharias.maladroit@gmail.com>

--- fs/fuse/dir.c
+++ fs/fuse/dir.c
@@ -1792,7 +1792,7 @@ int fuse_do_setattr(struct inode *inode, struct iattr *attr,
 	 */
 	if ((is_truncate || !is_wb) &&
 	    S_ISREG(inode->i_mode) && oldsize != outarg.attr.size) {
-		truncate_pagecache(inode, oldsize, outarg.attr.size);
+		truncate_pagecache(inode, outarg.attr.size);
 		invalidate_inode_pages2(inode->i_mapping);
 	}
 
