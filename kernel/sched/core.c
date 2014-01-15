d6a7c4ebefa5517df793f5959649fb750a3b32f5
Peter Zijlstra <peterz@infradead.org>
2014-01-15 12:30:15 +0100

    sched: Preserve the nice level over sched_setscheduler() and sched_setparam() calls
    Previously sched_setscheduler() and sched_setparam() would not affect
    the nice value of a task, restore this behaviour.
    
    Signed-off-by: Peter Zijlstra <peterz@infradead.org>
    Cc: raistlin@linux.it
    Cc: juri.lelli@gmail.com
    Cc: Michael wang <wangyun@linux.vnet.ibm.com>
    Cc: Daniel Lezcano <daniel.lezcano@linaro.org>
    Fixes: d50dde5a10f3 ("sched: Add new scheduler syscalls to support an extended scheduling parameters ABI")
    Link: http://lkml.kernel.org/r/20140115113015.GB31570@twins.programming.kicks-ass.net
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Git-Commit: e3de300d1212b42aa9d0d6031b12fca06ac00dd9
    Git-Repo: git://git.kernel.org/cgit/linux/kernel/git/torvalds/linux.git
    Signed-off-by: Joonwoo Park <joonwoop@codeaurora.org>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -5916,7 +5916,8 @@ int sched_setscheduler(struct task_struct *p, int policy,
 {
 	struct sched_attr attr = {
 		.sched_policy   = policy,
-		.sched_priority = param->sched_priority
+		.sched_priority = param->sched_priority,
+		.sched_nice	= PRIO_TO_NICE(p->static_prio),
 	};
 	return __sched_setscheduler(p, &attr, true);
 }
@@ -5944,7 +5945,8 @@ int sched_setscheduler_nocheck(struct task_struct *p, int policy,
 {
 	struct sched_attr attr = {
 		.sched_policy   = policy,
-		.sched_priority = param->sched_priority
+		.sched_priority = param->sched_priority,
+		.sched_nice	= PRIO_TO_NICE(p->static_prio),
 	};
 	return __sched_setscheduler(p, &attr, false);
 }
