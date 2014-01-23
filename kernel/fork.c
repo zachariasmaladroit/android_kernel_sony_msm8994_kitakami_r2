ce25268cbbb9005b8ab4b8a88e852af3ad995f20
Daeseok Youn <daeseok.youn@gmail.com>
2014-01-23 15:55:48 -0800

    kernel/fork.c: remove redundant NULL check in dup_mm()
    current->mm doesn't need a NULL check in dup_mm().  Becasue dup_mm() is
    used only in copy_mm() and current->mm is checked whether it is NULL or
    not in copy_mm() before calling dup_mm().
    
    Signed-off-by: Daeseok Youn <daeseok.youn@gmail.com>
    Acked-by: Oleg Nesterov <oleg@redhat.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- kernel/fork.c
+++ kernel/fork.c
@@ -854,9 +854,6 @@ static struct mm_struct *dup_mm(struct task_struct *tsk)
 	struct mm_struct *mm, *oldmm = current->mm;
 	int err;
 
-	if (!oldmm)
-		return NULL;
-
 	mm = allocate_mm();
 	if (!mm)
 		goto fail_nomem;
