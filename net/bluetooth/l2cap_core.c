18292d92e1421f0c70c0feaacd9be16fc2eaa78c
Kamal Negi <kamaln@codeaurora.org>
2015-03-24 12:16:27 +0530

    bluetooth: Initiate authentication before sending l2cap connect response.
    Check and inititiate, if necessary, authentication procedure for
    the connection before sending l2cap connect response. For legacy
    devices, this pacth will initiate authentication procedure in the
    first l2cap connect request as legacy do not initiate authentication
    after connection.
    
    Signed-off-by: Kamal Negi <kamaln@codeaurora.org>
    
    Conflicts:
    	net/bluetooth/l2cap_core.c
    
    Signed-off-by: zachariasmaladroit <zacharias.maladroit@gmail.com>

--- net/bluetooth/l2cap_core.c
+++ net/bluetooth/l2cap_core.c
@@ -3624,7 +3624,9 @@ void __l2cap_connect_rsp_defer(struct l2cap_chan *chan)
 	else
 		rsp_code = L2CAP_CONN_RSP;
 
-	BT_DBG("chan %p rsp_code %u", chan, rsp_code);
+	l2cap_chan_check_security(chan);
+
+	BT_DBG("chan %pK rsp_code %u", chan, rsp_code);
 
 	l2cap_send_cmd(conn, chan->ident, rsp_code, sizeof(rsp), &rsp);
 
