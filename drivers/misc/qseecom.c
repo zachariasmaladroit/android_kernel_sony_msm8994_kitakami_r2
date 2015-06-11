75f1fc3275483d4f39ccc619763e936d3fcf3f7f
Zhen Kong <zkong@codeaurora.org>
2015-06-11 12:08:41 -0700

    qseecom: Do not try to load commonlib when loaded in lk
    Remove the loading of commonlib when loaded by lk
    
    Change-Id: Id8fcce391fc313fa6f2cbfa483358a0e73704895
    Acked-by: Baranidharan Muthukumaran <bmuthuku@qti.qualcomm.com>
    Signed-off-by: Zhen Kong <zkong@codeaurora.org>

--- drivers/misc/qseecom.c
+++ drivers/misc/qseecom.c
@@ -6068,6 +6068,8 @@ static int qseecom_probe(struct platform_device *pdev)
 				goto exit_deinit_clock;
 			}
 		}
+		if(qseecom.appsbl_qseecom_support)
+			qseecom.commonlib_loaded = true;
 	} else {
 		qseecom_platform_support = (struct msm_bus_scale_pdata *)
 						pdev->dev.platform_data;
