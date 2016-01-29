d9ff9fcc8455a99fc4bfda27a8bc6b30b6459a43
andip71 <andreasp@gmx.de>
2016-01-29 14:38:08 +0100

    cpufreq: ondemand: add support for saving tunables per policy governor instance
    credits to ZaneZam
    
    relevant parts repectfully borrowed from elementalx governor:
    https://github.com/zanezam/boeffla-kernel-oos-oneplus2/blob/boeffla/drivers/cpufreq/cpufreq_elementalx.c
    
    Conflicts:
    	drivers/cpufreq/cpufreq_governor.c
    	drivers/cpufreq/cpufreq_governor.h
    
    due to zzmoove & elementalx which also support it
    
    Signed-off-by: zachariasmaladroit <zacharias.maladroit@gmail.com>

--- drivers/cpufreq/cpufreq_governor.c
+++ drivers/cpufreq/cpufreq_governor.c
@@ -297,6 +297,8 @@ int cpufreq_governor_dbs(struct cpufreq_policy *policy,
 			rc = cdata->init_ex(dbs_data, policy);
 		} else if (cdata->governor == GOV_ZZMOOVE) {
 			rc = cdata->init_zz(dbs_data, policy);
+		} else if (cdata->governor == GOV_ONDEMAND) {
+			rc = cdata->init_od(dbs_data, policy);
 		} else {
 			rc = cdata->init(dbs_data);
 		}
