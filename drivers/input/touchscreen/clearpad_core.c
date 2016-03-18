9b7c0b7011eed73f3c2e32e56e95af85946f1659
Signed-off-by: angelsl <hidingfromhidden@gmail.com>
2016-03-18 22:28:06 +0100

    input: clearpad_core: Don't break when wakeup_gesture toggled during sleep Let the wakeup_gesture attribute affect the next suspend of the touchscreen, so that if wakeup_gesture is toggled while the touchscreen is suspended, the driver/touchscreen doesn't get confused (because the driver wakes it up assuming it is in normal suspend, but the touchscreen is in tap-to-wake mode -- or vice versa).
    This fixes the problem where phone calls cannot be answered with
    tap-to-wake on.
    
    Change-Id: I7fa5a826e84bd4a4b6cd25b8c249d2e83c628d3f
    Signed-off-by: angelsl <hidingfromhidden@gmail.com>
    (cherry picked from commit 6dc479f)
    
    adapted to kitakami source
    
    Signed-off-by: zachariasmaladroit <zacharias.maladroit@gmail.com>

--- drivers/input/touchscreen/clearpad_core.c
+++ drivers/input/touchscreen/clearpad_core.c
@@ -498,6 +498,7 @@ struct clearpad_cover_t {
 struct clearpad_wakeup_gesture_t {
 	bool supported;
 	bool enabled;
+	bool engaged;
 	bool lpm_disabled;
 	bool large_panel;
 	unsigned long time_started;
@@ -2330,7 +2331,7 @@ static int clearpad_set_suspend_mode(struct clearpad_t *this)
 
 	dev_dbg(&this->pdev->dev, "%s\n", __func__);
 
-	if (this->wakeup_gesture.enabled) {
+	if (this->wakeup_gesture.engaged) {
 		if (clearpad_is_valid_function(this, SYN_F11_2D)) {
 			rc = clearpad_put_bit(SYNF(this, F11_2D, CTRL, 0x00),
 				XY_REPORTING_MODE_WAKEUP_GESTURE_MODE,
@@ -2354,7 +2355,6 @@ static int clearpad_set_suspend_mode(struct clearpad_t *this)
 				"failed to enter wake-up gesture mode\n");
 			goto exit;
 		}
-
 		this->wakeup_gesture.time_started = jiffies - 1;
 		clearpad_set_delay(10);
 		LOG_CHECK(this, "enter doze mode\n");
@@ -2363,6 +2363,7 @@ static int clearpad_set_suspend_mode(struct clearpad_t *this)
 			if (rc)
 				goto exit;
 		}
+		this->wakeup_gesture.enabled = true;
 		this->wakeup_gesture.suspend_with_enabled = true;
 	} else {
 		rc = clearpad_put_bit(SYNF(this, F01_RMI, CTRL, 0x00),
@@ -2379,6 +2380,7 @@ static int clearpad_set_suspend_mode(struct clearpad_t *this)
 		rc = clearpad_vreg_suspend(this, 1);
 		if (rc)
 			goto exit;
+		this->wakeup_gesture.enabled = false;
 		this->wakeup_gesture.suspend_with_enabled = false;
 	}
 
@@ -3809,13 +3811,13 @@ static ssize_t clearpad_wakeup_gesture_store(struct device *dev,
 		goto exit;
 	}
 
-	this->wakeup_gesture.enabled = sysfs_streq(buf, "0") ? false : true;
+	this->wakeup_gesture.engaged = sysfs_streq(buf, "0") ? false : true;
 
 	device_init_wakeup(&this->pdev->dev,
-			this->wakeup_gesture.enabled ? 1 : 0);
+			this->wakeup_gesture.engaged ? 1 : 0);
 
 	dev_info(&this->pdev->dev, "wakeup gesture: %s",
-			this->wakeup_gesture.enabled ? "ENABLE" : "DISABLE");
+			this->wakeup_gesture.engaged ? "ENABLE" : "DISABLE");
 exit:
 	UNLOCK(this);
 
