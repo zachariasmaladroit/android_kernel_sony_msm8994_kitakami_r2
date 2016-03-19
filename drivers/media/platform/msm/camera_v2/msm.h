6f7da6c0b8f1f21b9e3e59d1ca516be8025c9fc9
zachariasmaladroit <zacharias.maladroit@gmail.com>
2016-03-19 18:53:46 +0100

    Revert "power:Enable/Disable power collapse after camera open"
    This reverts commit 55336f05b3d9e84cb94fde1b8087accaedc25354.

--- drivers/media/platform/msm/camera_v2/msm.h
+++ drivers/media/platform/msm/camera_v2/msm.h
@@ -43,9 +43,6 @@
 #define MSM_POST_EVT_NOTIMEOUT 0xFFFFFFFF
 #define MSM_CAMERA_STREAM_CNT_BITS  32
 
-#define CAMERA_DISABLE_PC_LATENCY 100
-#define CAMERA_ENABLE_PC_LATENCY PM_QOS_DEFAULT_VALUE
-
 struct msm_video_device {
 	struct video_device *vdev;
 	atomic_t opened;
@@ -114,13 +111,6 @@ struct msm_session {
 	struct mutex lock;
 };
 
-<<<<<<< HEAD
-=======
-void msm_pm_qos_update_request(int val);
-
-int msm_cam_get_module_init_status(void);
-int msm_module_init_status(void);
->>>>>>> 099240c... power:Enable/Disable power collapse after camera open
 int msm_post_event(struct v4l2_event *event, int timeout);
 int  msm_create_session(unsigned int session, struct video_device *vdev);
 int msm_destroy_session(unsigned int session_id);
