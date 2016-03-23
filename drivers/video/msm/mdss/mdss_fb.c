4d1bb2cce1dac9799a5944871ff1386568690807
zachariasmaladroit <zacharias.maladroit@gmail.com>
2016-03-23 17:08:03 +0100

    Revert "msm: Use ammended interruptible threads"
    This reverts commit c660ab14fc03c55e67daf4d4d95c2eb4d483ca8c.

--- drivers/video/msm/mdss/mdss_fb.c
+++ drivers/video/msm/mdss/mdss_fb.c
@@ -3291,10 +3291,9 @@ static int __mdss_fb_display_thread(void *data)
 				mfd->index);
 
 	while (1) {
-		while (wait_event_interruptible(
-			mfd->commit_wait_q,
-			(atomic_read(&mfd->commits_pending) ||
-			kthread_should_stop())) != 0);
+		wait_event(mfd->commit_wait_q,
+				(atomic_read(&mfd->commits_pending) ||
+				 kthread_should_stop()));
 
 		if (kthread_should_stop())
 			break;
