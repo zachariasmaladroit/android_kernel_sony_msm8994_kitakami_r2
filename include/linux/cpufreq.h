d37c8a07ba6bedc1485e111390494594e4e26180
maxwen <max.weninger@gmail.com>
2016-03-31 10:04:55 +0200

    add governor smartmax
    Conflicts:
    	drivers/cpufreq/Kconfig
    	drivers/cpufreq/Makefile
    	include/linux/cpufreq.h
    
    Signed-off-by: zachariasmaladroit <zacharias.maladroit@gmail.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -479,6 +479,9 @@ extern struct cpufreq_governor cpufreq_gov_lionheart;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_BARRY_ALLEN)
 extern struct cpufreq_governor cpufreq_gov_barry_allen;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_barry_allen)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SMARTMAX)
+extern struct cpufreq_governor cpufreq_gov_smartmax;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_smartmax)
 #endif
 
 /*********************************************************************
