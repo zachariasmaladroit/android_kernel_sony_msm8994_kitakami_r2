3a785f0307974e7db7f52bf7c8567b57891f1596
jollaman999 <admin@jollaman999.com>
2016-05-21 02:27:23 +0900

    bcl, cpu: Skip cpu_up if offlined by bcl_handle_hotplug

--- kernel/cpu.c
+++ kernel/cpu.c
@@ -28,6 +28,10 @@
 #include <linux/msm_thermal.h>
 #endif
 
+#ifdef CONFIG_MSM_BCL_CTL
+#include <linux/msm_bcl.h>
+#endif
+
 #ifdef CONFIG_SMP
 /* Serializes the updates to cpu_online_mask, cpu_present_mask */
 static DEFINE_MUTEX(cpu_add_remove_lock);
@@ -455,6 +459,11 @@ int __cpuinit cpu_up(unsigned int cpu)
 
 #ifdef CONFIG_THERMAL_MONITOR
 	if (cpus_offlined & BIT(cpu))
+		return 0;
+#endif
+
+#ifdef CONFIG_MSM_BCL_CTL
+	if (bcl_hotplug_request & BIT(cpu))
 		return 0;
 #endif
 
