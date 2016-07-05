28dc3613da18bd5e6af5fe95614407b89870c091
zachariasmaladroit <zacharias.maladroit@gmail.com>
2016-07-05 21:27:27 +0700

    cpu: keep cpu0 and cpu4 always on

--- kernel/cpu.c
+++ kernel/cpu.c
@@ -371,6 +371,13 @@ int __ref cpu_down(unsigned int cpu)
 
 	cpu_maps_update_begin();
 
+	/* Keep cpu0 and cpu4 always on */
+	if (cpu == 0 || cpu == 4)
+	{
+		err = -EBUSY;
+		goto out;
+	}
+
 	if (cpu_hotplug_disabled) {
 		err = -EBUSY;
 		goto out;
