060d488887fb4272a065dceb97158818bea229e3
Joe Maples <joe@frap129.org>
2016-07-10 17:26:44 +0000

    cpufreq: Add Chill cpu gov
    Signed-off-by: Joe Maples <joe@frap129.org>
    [@nathanchance: added default governor header in cpufreq_governor.h and updated for dwork]
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>
    
    Conflicts:
    	drivers/cpufreq/Makefile
    	drivers/cpufreq/cpufreq_governor.h
    
    Signed-off-by: zachariasmaladroit <zacharias.maladroit@gmail.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -444,6 +444,9 @@ extern struct cpufreq_governor cpufreq_gov_alucard;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_FLASH)
 extern struct cpufreq_governor cpufreq_gov_flash;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_flash)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_CHILL)
+extern struct cpufreq_governor cpufreq_gov_chill;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_chill)
 #endif
 
 /*********************************************************************
