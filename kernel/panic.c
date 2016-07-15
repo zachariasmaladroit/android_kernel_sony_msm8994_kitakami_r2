c08ac8e7a2f7049d8bc17ea3fbcab5075a1ffcd4
Tejun Heo <tj@kernel.org>
2016-07-15 15:08:20 -0400

    printk: do cond_resched() between lines while outputting to consoles
    commit 8d91f8b15361dfb438ab6eb3b319e2ded43458ff upstream.
    
    @console_may_schedule tracks whether console_sem was acquired through
    lock or trylock.  If the former, we're inside a sleepable context and
    console_conditional_schedule() performs cond_resched().  This allows
    console drivers which use console_lock for synchronization to yield
    while performing time-consuming operations such as scrolling.
    
    However, the actual console outputting is performed while holding
    irq-safe logbuf_lock, so console_unlock() clears @console_may_schedule
    before starting outputting lines.  Also, only a few drivers call
    console_conditional_schedule() to begin with.  This means that when a
    lot of lines need to be output by console_unlock(), for example on a
    console registration, the task doing console_unlock() may not yield for
    a long time on a non-preemptible kernel.
    
    If this happens with a slow console devices, for example a serial
    console, the outputting task may occupy the cpu for a very long time.
    Long enough to trigger softlockup and/or RCU stall warnings, which in
    turn pile more messages, sometimes enough to trigger the next cycle of
    warnings incapacitating the system.
    
    Fix it by making console_unlock() insert cond_resched() between lines if
    @console_may_schedule.
    
    Signed-off-by: Tejun Heo <tj@kernel.org>
    Reported-by: Calvin Owens <calvinowens@fb.com>
    Acked-by: Jan Kara <jack@suse.com>
    Cc: Dave Jones <davej@codemonkey.org.uk>
    Cc: Kyle McMartin <kyle@kernel.org>
    Cc: <stable@vger.kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    [ciwillia@brocade.com: adjust context for 3.10.y]
    Signed-off-by: Chas Williams <ciwillia@brocade.com>
    Signed-off-by: Willy Tarreau <w@1wt.eu>
    
    Conflicts:
    	kernel/panic.c
    
    Signed-off-by: zachariasmaladroit <zacharias.maladroit@gmail.com>

--- kernel/panic.c
+++ kernel/panic.c
@@ -29,7 +29,6 @@
 #include <linux/nmi.h>
 #include <linux/crash_notes.h>
 #include <linux/console.h>
-
 #define CREATE_TRACE_POINTS
 #include <trace/events/exception.h>
 
@@ -154,6 +153,8 @@ void panic(const char *fmt, ...)
 	atomic_notifier_call_chain(&panic_notifier_list, 0, buf);
 
 	bust_spinlocks(0);
+
+	console_flush_on_panic();
 
 	if (!panic_blink)
 		panic_blink = no_blink;
