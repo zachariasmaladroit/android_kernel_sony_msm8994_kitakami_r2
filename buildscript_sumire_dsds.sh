dcff91a7f758eb6cf772cdd812ddad03507a4537
RyTek <rytek1128@outlook.com>
2016-07-26 08:33:27 +0700

    sumire: Higher charging limitation

--- buildscript_sumire_dsds.sh
+++ buildscript_sumire_dsds.sh
@@ -16,7 +16,7 @@ then
 	echo "DONE"
 	rm -f ../final_files/boot_E6683.img
 
-	../final_files/mkbootimg --kernel arch/arm64/boot/Image.gz-dtb --ramdisk ../final_files/newrd.gz --cmdline "androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 boot_cpus=0-5 dwc3_msm.prop_chg_detect=Y coherent_pool=2M dwc3_msm.hvdcp_max_current=1500 androidboot.selinux=permissive enforcing=0" --base 0x00000000 --pagesize 4096 --ramdisk_offset 0x02000000 --tags_offset 0x01E00000 --output ../final_files/boot_E6683.img
+	../final_files/mkbootimg --kernel arch/arm64/boot/Image.gz-dtb --ramdisk ../final_files/newrd.gz --cmdline "androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 boot_cpus=0-5 dwc3_msm.prop_chg_detect=Y coherent_pool=2M dwc3_msm.hvdcp_max_current=1800 androidboot.selinux=permissive enforcing=0" --base 0x00000000 --pagesize 4096 --ramdisk_offset 0x02000000 --tags_offset 0x01E00000 --output ../final_files/boot_E6683.img
 
 	make mrproper
 
