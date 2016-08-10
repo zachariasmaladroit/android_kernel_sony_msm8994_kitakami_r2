dd0637eab17975c1dfd095dcf311ce383e8a1beb
Humberto Borba <humberos@gmail.com>
2016-08-10 20:54:08 -0300

    drivers: staging: android: Optmize the use of mutex
    Use the same mutex for ion_handle_*
    It was extracted from Sony 34.0.A.x kernel sources.
    
    Signed-off-by: Humberto Borba <humberos@gmail.com>
    Change-Id: I20bbf8a656b2cc4189c8b4516ee16eff842790f2

--- drivers/staging/android/ion/ion.c
+++ drivers/staging/android/ion/ion.c
@@ -4,6 +4,7 @@
  *
  * Copyright (C) 2011 Google, Inc.
  * Copyright (c) 2011-2015, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2015 Sony Mobile Communications Inc
  *
  * This software is licensed under the terms of the GNU General Public
  * License version 2, as published by the Free Software Foundation, and
