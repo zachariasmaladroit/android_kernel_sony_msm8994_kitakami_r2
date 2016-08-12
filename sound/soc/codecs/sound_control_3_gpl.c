0610d85c38dd85a5e06f0053f37b9c4675336fe6
zachariasmaladroit <zacharias.maladroit@gmail.com>
2016-08-12 02:10:08 +0200

    Revert "sound/fauxsound: Updates"
    This reverts commit 92c72f8651d64e31e18ae79d90cd6af905bdf690.

--- sound/soc/codecs/sound_control_3_gpl.c
+++ sound/soc/codecs/sound_control_3_gpl.c
@@ -195,8 +195,6 @@ static bool calc_checksum(unsigned int a, unsigned int b, unsigned int c)
 	unsigned char chksum = 0;
 
 	chksum = ~((a & 0xff) + (b & 0xff));
-	
-	return true;
 
 	if (chksum == (c & 0xff)) {
 		return true;
@@ -265,15 +263,9 @@ static ssize_t speaker_gain_show(struct kobject *kobj,
 static ssize_t speaker_gain_store(struct kobject *kobj,
 		struct kobj_attribute *attr, const char *buf, size_t count)
 {
-	int lval, rval, chksum;
+	unsigned int lval, rval, chksum;
 
-	sscanf(buf, "%i %i %i", &lval, &rval, &chksum);
-	
-	if (lval < 0)
-		lval = 0;
-	
-	if (rval < 0)
-		rval = 0;
+	sscanf(buf, "%u %u %u", &lval, &rval, &chksum);
 
 	if (calc_checksum(lval, rval, chksum)) {
 		tomtom_write(fauxsound_codec_ptr,
@@ -297,15 +289,9 @@ static ssize_t headphone_gain_show(struct kobject *kobj,
 static ssize_t headphone_gain_store(struct kobject *kobj,
 		struct kobj_attribute *attr, const char *buf, size_t count)
 {
-	int lval, rval, chksum;
+	unsigned int lval, rval, chksum;
 
-	sscanf(buf, "%i %i %i", &lval, &rval, &chksum);
-	
-	if (lval < 0)
-		lval = 0;
-	
-	if (rval < 0)
-		rval = 0;
+	sscanf(buf, "%u %u %u", &lval, &rval, &chksum);
 
 	if (calc_checksum(lval, rval, chksum)) {
 		tomtom_write(fauxsound_codec_ptr,
@@ -327,17 +313,11 @@ static ssize_t headphone_pa_gain_show(struct kobject *kobj,
 static ssize_t headphone_pa_gain_store(struct kobject *kobj,
 		struct kobj_attribute *attr, const char *buf, size_t count)
 {
-	int lval, rval, chksum;
+	unsigned int lval, rval, chksum;
 	unsigned int gain, status;
 	unsigned int out;
 
-	sscanf(buf, "%i %i %i", &lval, &rval, &chksum);
-	
-	if (lval < 0)
-		lval = 0;
-	
-	if (rval < 0)
-		rval = 0;
+	sscanf(buf, "%u %u %u", &lval, &rval, &chksum);
 
 	if (calc_checksum(lval, rval, chksum)) {
 	gain = tomtom_read(fauxsound_codec_ptr, TOMTOM_A_RX_HPH_L_GAIN);
