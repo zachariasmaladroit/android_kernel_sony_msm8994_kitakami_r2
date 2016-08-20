44948482d8aea46b5a8f8ccf5dcc7f32a6358911
Signed-off-by: Alex Naidis <alex.naidis@linux.com>
2016-08-20 21:22:58 +0200

    cpufreq: cpu-boost: disregard Big Cluster
    * We shouldn't be boosting BIG cores
    * not even consider it
    
    Change-Id: I0e58d17549f1861cbc1b648622694f7fe4dbb8e9
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>
    
    adapted to work with modified cpu-boost
    
    Signed-off-by: zachariasmaladroit <zacharias.maladroit@gmail.com>

--- drivers/cpufreq/cpu-boost.c
+++ drivers/cpufreq/cpu-boost.c
@@ -183,6 +183,9 @@ static int boost_adjust_notify(struct notifier_block *nb, unsigned long val,
 	if (!b_min && !ib_min)
 		return NOTIFY_OK;
 
+	if (cpu >= 4)
+		return NOTIFY_OK;
+
 	min = max(b_min, ib_min);
 	min = min(min, policy->max);
 
@@ -221,7 +224,8 @@ static void update_policy_online(void)
 	get_online_cpus();
 	for_each_online_cpu(i) {
 		pr_debug("Updating policy for CPU%d\n", i);
-		cpufreq_update_policy(i);
+		if (i <= 3)
+			cpufreq_update_policy(i);
 	}
 	put_online_cpus();
 }
