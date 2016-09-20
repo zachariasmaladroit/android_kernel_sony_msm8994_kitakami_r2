958e08a8f94edddb624bdd6fb6bb077b39ee2b21
RyTek <rytek1128@outlook.com>
2016-09-20 22:07:27 +0700

    module: Don't force load core_ctl module

--- kernel/module.c
+++ kernel/module.c
@@ -1180,8 +1180,8 @@ static int check_version(Elf_Shdr *sechdrs,
 	if(!strncmp("ecryptfs", mod->name, 8))
 		return 1;
 
-	if(!strncmp("core_ctl", mod->name, 8))
-		return 1;
+//	if(!strncmp("core_ctl", mod->name, 8))
+//		return 1;
 
 	/* Exporting module didn't supply crcs?  OK, we're already tainted. */
 	if (!crc)
