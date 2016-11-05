331eb8c80d9a3daf4869eb321e066bda817fc863
zachariasmaladroit <zacharias.maladroit@gmail.com>
2016-11-05 23:11:44 +0100

    Revert "msm: thermal: Use phandle to input the cluster cpu map"
    This reverts commit 1d97bcca48a83e4ec5bbcf126dd8d9cb3796bc76.

--- drivers/thermal/msm_thermal.c
+++ drivers/thermal/msm_thermal.c
@@ -1012,64 +1012,40 @@ static int create_cpu_topology_sysfs(void)
 static int get_device_tree_cluster_info(struct device *dev, int *cluster_id,
 			cpumask_t *cluster_cpus)
 {
-	int idx = 0, ret = 0, max_entry = 0, core_cnt = 0, c_idx = 0, cpu = 0;
+	int i, cluster_cnt = 0, ret = 0;
 	uint32_t val = 0;
 	char *key = "qcom,synchronous-cluster-map";
-	struct device_node *core_phandle = NULL;
 
-	if (!of_get_property(dev->of_node, key, &max_entry)
-		|| max_entry <= 0) {
+	if (!of_get_property(dev->of_node, key, &cluster_cnt)
+		|| cluster_cnt <= 0) {
 		pr_debug("Property %s not defined.\n", key);
 		return -ENODEV;
 	}
-	max_entry /= sizeof(__be32);
+	if (cluster_cnt % (sizeof(__be32) * 2)) {
+		pr_err("Invalid number(%d) of entry for %s\n",
+				cluster_cnt, key);
+		return -EINVAL;
+	}
+	cluster_cnt /= (sizeof(__be32) * 2);
 
-	for (idx = 0; idx < max_entry; idx++, c_idx++) {
-		/* Read Cluster ID */
-		ret = of_property_read_u32_index(dev->of_node, key, idx++,
-			&val);
+	for (i = 0; i < cluster_cnt; i++) {
+		ret = of_property_read_u32_index(dev->of_node, key,
+							i * 2, &val);
 		if (ret) {
-			pr_err("Error reading index%d. err:%d\n", idx - 1,
-				ret);
+			pr_err("Error reading index%d\n", i * 2);
 			return -EINVAL;
 		}
-		/* Read number of cores inside a cluster */
-		cluster_id[c_idx] = val;
-		cpumask_clear(&cluster_cpus[c_idx]);
-		ret = of_property_read_u32_index(dev->of_node, key, idx,
-			&val);
-		if (ret || val < 1) {
-			pr_err("Invalid core count[%d] for Cluster%d. err:%d\n"
-					, val, cluster_id[c_idx - 1], ret);
-			return -EINVAL;
-		}
-		core_cnt = val + idx;
-		/* map the cores to logical CPUs and get sibiling mask */
-		for (; core_cnt != idx; core_cnt--) {
-			core_phandle = of_parse_phandle(dev->of_node, key,
-						core_cnt);
-			if (!core_phandle) {
-				pr_debug("Invalid phandle. core%d cluster%d\n",
-					core_cnt, cluster_id[c_idx - 1]);
-				continue;
-			}
+		cluster_id[i] = val;
 
-			for_each_possible_cpu(cpu) {
-				if (of_get_cpu_node(cpu, NULL)
-					== core_phandle)
-					break;
-			}
-			if (cpu >= num_possible_cpus()) {
-				pr_debug("Skipping core%d in cluster%d\n",
-					core_cnt, cluster_id[c_idx - 1]);
-				continue;
-			}
-			cpumask_set_cpu(cpu, &cluster_cpus[c_idx]);
+		of_property_read_u32_index(dev->of_node, key, i * 2 + 1, &val);
+		if (ret) {
+			pr_err("Error reading index%d\n", i * 2 + 1);
+			return -EINVAL;
 		}
-		idx += val;
+		*cluster_cpus[i].bits = val;
 	}
 
-	return c_idx;
+	return cluster_cnt;
 }
 
 static int get_kernel_cluster_info(int *cluster_id, cpumask_t *cluster_cpus)
@@ -1092,7 +1068,7 @@ static int get_kernel_cluster_info(int *cluster_id, cpumask_t *cluster_cpus)
 			if (cluster_id[cluster_index] == -1) {
 				cluster_id[cluster_index] =
 					topology_physical_package_id(_cpu);
-				cpumask_clear(&cluster_cpus[cluster_index]);
+				*cluster_cpus[cluster_index].bits = 0;
 				cpumask_set_cpu(_cpu,
 					&cluster_cpus[cluster_index]);
 				cluster_cnt++;
@@ -1114,8 +1090,8 @@ static void update_cpu_topology(struct device *dev)
 {
 	int cluster_id[NR_CPUS] = {[0 ... NR_CPUS-1] = -1};
 	cpumask_t cluster_cpus[NR_CPUS];
-	uint32_t i;
-	int cluster_cnt, sync_cluster_cnt = 0;
+	uint32_t i, j;
+	int cluster_cnt, cpu, sync_cluster_cnt = 0;
 	struct cluster_info *temp_ptr = NULL;
 	int *sync_cluster_id = NULL;
 
@@ -1158,17 +1134,20 @@ static void update_cpu_topology(struct device *dev)
 
 	for (i = 0; i < cluster_cnt; i++) {
 		pr_debug("Cluster_ID:%d CPU's:%lu\n", cluster_id[i],
-				*cpumask_bits(&cluster_cpus[i]));
+				*cluster_cpus[i].bits);
 		temp_ptr[i].cluster_id = cluster_id[i];
 		temp_ptr[i].parent_ptr = core_ptr;
-		cpumask_copy(&temp_ptr[i].cluster_cores, &cluster_cpus[i]);
+		temp_ptr[i].cluster_cores = cluster_cpus[i];
 		temp_ptr[i].limited_max_freq = UINT_MAX;
 		temp_ptr[i].limited_min_freq = 0;
 		temp_ptr[i].freq_idx = 0;
 		temp_ptr[i].freq_idx_low = 0;
 		temp_ptr[i].freq_idx_high = 0;
 		temp_ptr[i].freq_table = NULL;
-		temp_ptr[i].entity_count = cpumask_weight(&cluster_cpus[i]);
+		j = 0;
+		for_each_cpu_mask(cpu, cluster_cpus[i])
+			j++;
+		temp_ptr[i].entity_count = j;
 		temp_ptr[i].child_entity_ptr = NULL;
 		update_cpu_datastructure(&temp_ptr[i], sync_cluster_id,
 				sync_cluster_cnt);
