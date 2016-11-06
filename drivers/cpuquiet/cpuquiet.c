c435cd8945ff97d6ca6b703ee617808059c76412
RyTek <rytek1128@outlook.com>
2016-11-06 21:11:26 +0700

    cpuquiet: do not allow CPU4 offline
    To prevent blank page or unrecognized settings for big cluster in kernel manage apps.
    Signed-off-by: RyTek <rytek1128@outlook.com>

--- drivers/cpuquiet/cpuquiet.c
+++ drivers/cpuquiet/cpuquiet.c
@@ -175,10 +175,12 @@ static void __cpuinit cpuquiet_work_func(struct work_struct *work)
 
 	mutex_unlock(&cpuquiet_cpu_lock);
 
-	/* always keep CPU0 online */
+	/* always keep CPU0&4 online */
 	cpumask_set_cpu(0, &online);
+	cpumask_set_cpu(4, &online);
 	cpu_online = *cpu_online_mask;
 	cpumask_clear_cpu(0, &offline);
+	cpumask_clear_cpu(4, &offline);
 
 	if (max_cpus < min_cpus)
 		max_cpus = min_cpus;
