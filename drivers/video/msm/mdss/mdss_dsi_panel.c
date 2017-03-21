50e32788c4dbddf166528756df7c5fb3320cf6f5
Sultanxda <sultanxda@gmail.com>
2017-03-21 10:12:10 -0700

    msm: mdss: Don't cache the address of a stack variable in timings init
    When the panel timings are parsed, a stack-allocated variable is used to
    store the parsed data; however, this creates two problems. The first
    problem is that this creates a memory leak since a kstrdup() address is
    stored into the stack-allocated variable at the end of the
    mdss_dsi_panel_timing_from_dt() function. The second problem this creates
    is that the address of the stack-allocated variable is stored into the
    current_timing struct member (inside mdss_dsi_panel_timing_switch()) for
    future use in the driver.
    
    Since the data that current_timing points to is expected to persist long
    after init, allocate memory for the timing settings to fix the issues.
    
    Signed-off-by: Sultanxda <sultanxda@gmail.com>
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>
    
    # Conflicts:
    #	drivers/video/msm/mdss/mdss_dsi_panel.c

--- drivers/video/msm/mdss/mdss_dsi_panel.c
+++ drivers/video/msm/mdss/mdss_dsi_panel.c
@@ -1650,8 +1650,11 @@ static int mdss_dsi_panel_parse_display_timings(struct device_node *np,
 
 	timings_np = of_get_child_by_name(np, "qcom,mdss-dsi-display-timings");
 	if (!timings_np) {
-		struct dsi_panel_timing pt;
-		memset(&pt, 0, sizeof(struct dsi_panel_timing));
+		struct dsi_panel_timing *pt;
+
+		pt = kzalloc(sizeof(*pt), GFP_KERNEL);
+		if (!pt)
+			return -ENOMEM;
 
 		/*
 		 * display timings node is not available, fallback to reading
@@ -1663,6 +1666,8 @@ static int mdss_dsi_panel_parse_display_timings(struct device_node *np,
 			mdss_dsi_panel_config_res_properties(np,
 				panel_data->panel_info.sim_panel_mode, &pt);
 			rc = mdss_dsi_panel_timing_switch(ctrl, &pt.timing);
+		} else {
+			kfree(pt);
 		}
 		return rc;
 	}
