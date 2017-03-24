4d9a5df4d53ebdaee2885390d44ac2cbe59bf320
zachariasmaladroit <zacharias.maladroit@gmail.com>
2017-03-24 20:47:37 +0100

    cpu-boost: raise input_boost_ms to 1000 (1s) to smooth out the UI

--- drivers/cpufreq/cpu-boost.c
+++ drivers/cpufreq/cpu-boost.c
@@ -63,7 +63,7 @@ module_param(sync_threshold, uint, 0644);
 static unsigned int input_boost_enabled = 1;
 module_param(input_boost_enabled, uint, 0644);
 
-static unsigned int input_boost_ms = 40;
+static unsigned int input_boost_ms = 1000;
 module_param(input_boost_ms, uint, 0644);
 
 static unsigned int migration_load_threshold = 15;
