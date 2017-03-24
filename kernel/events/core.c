db8f732bb71231d3b8e9c05d3f9841d30ee67666
zachariasmaladroit <zacharias.maladroit@gmail.com>
2017-03-24 17:49:47 +0100

    Revert "msm: null pointer dereferencing"
    This reverts commit 2ef889700e6bc9b6c8ac0c2144ee74383b27c4b9.

--- kernel/events/core.c
+++ kernel/events/core.c
@@ -5704,8 +5704,7 @@ static int perf_swevent_add(struct perf_event *event, int flags)
 
 static void perf_swevent_del(struct perf_event *event, int flags)
 {
-	if(!hlist_unhashed(&event->hlist_entry))
-		hlist_del_rcu(&event->hlist_entry);
+	hlist_del_rcu(&event->hlist_entry);
 }
 
 static void perf_swevent_start(struct perf_event *event, int flags)
