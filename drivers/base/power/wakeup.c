3967da3ab0d08886959c4406f1a43d75decd1d75
zachariasmaladroit <zacharias.maladroit@gmail.com>
2017-03-31 00:04:28 +0200

    Revert (remove) all occurences of 'power: validate wakeup source before activating it',
    fix up 'power: validate wakeup source before activating it'

--- drivers/base/power/wakeup.c
+++ drivers/base/power/wakeup.c
@@ -523,14 +523,6 @@ static void wakeup_source_activate(struct wakeup_source *ws)
 			"unregistered wakeup source\n"))
 		return;
 
-	if (WARN(wakeup_source_not_registered(ws),
-			"unregistered wakeup source\n"))
-		return;
-
-	if (WARN(wakeup_source_not_registered(ws),
-			"unregistered wakeup source\n"))
-		return;
-
 	if (((!enable_wlan_rx_wake_ws &&
 			!strncmp(ws->name, "wlan_rx_wake", 12)) ||
 		(!enable_si_ws &&
@@ -558,9 +550,6 @@ static void wakeup_source_activate(struct wakeup_source *ws)
 		return;
 	}
 
-	if (WARN(wakeup_source_not_registered(ws),
-			"unregistered wakeup source\n"))
-		return;
 	/*
 	 * active wakeup source should bring the system
 	 * out of PM_SUSPEND_FREEZE state
