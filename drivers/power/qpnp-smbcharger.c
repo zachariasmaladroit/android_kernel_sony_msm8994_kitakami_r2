92567c41ba76009965ff100646cf23f680e0b734
Francisco Franco <franciscofranco.1990@gmail.com>
2017-04-15 23:06:19 +0200

    [PORT] power: smbcharger: queue work on system_power_efficient_wq
    There doesn't seem to be any real dependency of scheduling these on
    the cpu which scheduled them, so moving every *_delayed to the
    power efficient wq save potential needlessly idle cpu wake ups
    leaving the scheduler to decide the most appropriate cpus to wake up.
    
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>
    
    Porting notes:
    
    leaving the default 'schedule_delayed_work' in place for the UV
    condition since these might warrant higher urgency
    
    Signed-off-by: zachariasmaladroit <zacharias.maladroit@gmail.com>

--- drivers/power/qpnp-smbcharger.c
+++ drivers/power/qpnp-smbcharger.c
@@ -2209,7 +2209,7 @@ static void smbchg_parallel_usb_check_ok(struct smbchg_chip *chip)
 	mutex_lock(&chip->parallel.lock);
 	if (smbchg_is_parallel_usb_ok(chip)) {
 		smbchg_stay_awake(chip, PM_PARALLEL_CHECK);
-		schedule_delayed_work(
+		queue_delayed_work(system_power_efficient_wq,
 			&chip->parallel_en_work,
 			msecs_to_jiffies(PARALLEL_CHARGER_EN_DELAY_MS));
 	} else if (chip->parallel.current_max_ma != 0) {
@@ -3009,7 +3009,8 @@ static void smbchg_vfloat_adjust_check(struct smbchg_chip *chip)
 
 	smbchg_stay_awake(chip, PM_REASON_VFLOAT_ADJUST);
 	pr_smb(PR_STATUS, "Starting vfloat adjustments\n");
-	schedule_delayed_work(&chip->vfloat_adjust_work, 0);
+	queue_delayed_work(system_power_efficient_wq,
+		&chip->vfloat_adjust_work, 0);
 }
 
 #define DC_AICL_CFG			0xF3
@@ -3947,8 +3948,9 @@ stop:
 	return;
 
 reschedule:
-	schedule_delayed_work(&chip->vfloat_adjust_work,
-			msecs_to_jiffies(VFLOAT_RESAMPLE_DELAY_MS));
+	queue_delayed_work(system_power_efficient_wq,
+		&chip->vfloat_adjust_work,
+		msecs_to_jiffies(VFLOAT_RESAMPLE_DELAY_MS));
 	return;
 }
 
@@ -4062,7 +4064,7 @@ static void handle_usb_removal(struct smbchg_chip *chip)
 	somc_unplug_wakelock();
 	somc_chg_invalid_set_state(&chip->somc_params, 0);
 	if (!is_dc_present(chip) && !is_usb_present(chip))
-		schedule_delayed_work(
+		queue_delayed_work(system_power_efficient_wq,
 				&chip->somc_params.chg_key.remove_work,
 				msecs_to_jiffies(REMOVE_DELAY_MS));
 	somc_llk_usbdc_present_chk(&chip->somc_params);
@@ -4160,8 +4162,9 @@ static void handle_usb_insertion(struct smbchg_chip *chip)
 	}
 
 	if (usb_supply_type == POWER_SUPPLY_TYPE_USB_DCP)
-		schedule_delayed_work(&chip->hvdcp_det_work,
-					msecs_to_jiffies(HVDCP_NOTIFY_MS));
+		queue_delayed_work(system_power_efficient_wq,
+				&chip->hvdcp_det_work,
+				msecs_to_jiffies(HVDCP_NOTIFY_MS));
 
 #ifndef CONFIG_QPNP_SMBCHARGER_EXTENSION
 	mutex_lock(&chip->current_change_lock);
