909dc140145160fc82ff036ad5791554ff2d30ec
zachariasmaladroit <zacharias.maladroit@gmail.com>
2017-04-18 03:22:03 +0200

    Revert "cpufreq: cpu-boost: don't boost big cluster on input touch unless it has at least 1 task running on any of its cores to save power"
    This reverts commit c32d847c6d3f71811a017028816d8e189b79e6c6.
    
    Conflicts:
    	drivers/cpufreq/cpu-boost.c

--- drivers/cpufreq/cpu-boost.c
+++ drivers/cpufreq/cpu-boost.c
@@ -31,8 +31,6 @@
 #include <linux/input.h>
 #include <linux/time.h>
 
-#include "../../kernel/sched/sched.h"
-
 struct cpu_sync {
 	struct task_struct *thread;
 	wait_queue_head_t sync_wq;
@@ -45,7 +43,6 @@ struct cpu_sync {
 	unsigned int input_boost_min;
 	unsigned int task_load;
 	unsigned int input_boost_freq;
-	unsigned int nr_running;
 };
 
 static DEFINE_PER_CPU(struct cpu_sync, sync_info);
@@ -79,8 +76,6 @@ static bool sched_boost_active;
 static struct delayed_work input_boost_rem;
 static u64 last_input_time;
 #define MIN_INPUT_INTERVAL (150 * USEC_PER_MSEC)
-
-static unsigned int cnt_nr_running;
 
 static int set_input_boost_freq(const char *buf, const struct kernel_param *kp)
 {
@@ -168,9 +163,6 @@ static int boost_adjust_notify(struct notifier_block *nb, unsigned long val,
 
 		min = max(b_min, ib_min);
 
-		if (cpu == 4 && min > 0 && cnt_nr_running == 0)
-                        break;
-
 		min = min(min, policy->max);
 
 		pr_debug("CPU%u policy min before boost: %u kHz\n",
@@ -215,8 +207,7 @@ static void update_policy_online(void)
 	get_online_cpus();
 	for_each_online_cpu(i) {
 		pr_debug("Updating policy for CPU%d\n", i);
-		if (i == 0 || i == 4)
-			cpufreq_update_policy(i);
+		cpufreq_update_policy(i);
 	}
 	put_online_cpus();
 }
@@ -232,8 +223,6 @@ static void do_input_boost_rem(struct work_struct *work)
 		i_sync_info = &per_cpu(sync_info, i);
 		i_sync_info->input_boost_min = 0;
 	}
-
-	cnt_nr_running = 0;
 
 	/* Update policies for all online CPUs */
 	update_policy_online();
@@ -374,9 +363,6 @@ static void do_input_boost(struct work_struct *work)
 	for_each_possible_cpu(i) {
 		i_sync_info = &per_cpu(sync_info, i);
 		i_sync_info->input_boost_min = i_sync_info->input_boost_freq;
-
-		if (i >= 4)
-			cnt_nr_running += cpu_rq(i)->nr_running;
 	}
 
 	/* Update policies for all online CPUs */
@@ -406,7 +392,7 @@ static void cpuboost_input_event(struct input_handle *handle,
 		return;
 
 	now = ktime_to_us(ktime_get());
-	if (now - last_input_time < (input_boost_ms * USEC_PER_MSEC))
+	if (now - last_input_time < MIN_INPUT_INTERVAL)
 		return;
 
 	if (work_pending(&input_boost_work))
