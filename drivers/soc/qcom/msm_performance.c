ca341c9228974cc7aefb9a83a7c5d1d963220fed
zachariasmaladroit <zacharias.maladroit@gmail.com>
2017-04-26 16:48:07 +0200

    Revert "msm_performance: Make input boosting optional"
    This reverts commit 395bd06c0d1adeefcd7cbe75d8ad9be182225fa3.

--- drivers/soc/qcom/msm_performance.c
+++ drivers/soc/qcom/msm_performance.c
@@ -378,10 +378,6 @@ static int set_cpu_min_freq(const char *buf, const struct kernel_param *kp)
 	struct cpufreq_policy policy;
 	cpumask_var_t limit_mask;
 	int ret;
-	const char *reset = "0:0 4:0";
-
-	if (touchboost == 0)
-		cp = reset;
 
 	while ((cp = strpbrk(cp + 1, " :")))
 		ntokens++;
@@ -390,11 +386,7 @@ static int set_cpu_min_freq(const char *buf, const struct kernel_param *kp)
 	if (!(ntokens % 2))
 		return -EINVAL;
 
-	if (touchboost == 0)
-		cp = reset;
-	else
-		cp = buf;
-
+	cp = buf;
 	cpumask_clear(limit_mask);
 	for (i = 0; i < ntokens; i += 2) {
 		if (sscanf(cp, "%u:%u", &cpu, &val) != 2)
