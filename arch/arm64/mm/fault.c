57af7d74fe127017ac63180fab8e6e082a30503e
Mark Salyzyn <salyzyn@android.com>
2017-05-07 15:37:39 +0200

    ARM64 readahead: fault retry breaks mmap file read random detection
    Description from commit 45cac65b0fcd
        ("readahead: fault retry breaks mmap file read random detection")
    
    .fault now can retry.  The retry can break state machine of .fault.  In
    filemap_fault, if page is miss, ra->mmap_miss is increased.  In the
    second
    try, since the page is in page cache now, ra->mmap_miss is decreased.
    And
    these are done in one fault, so we can't detect random mmap file access.
    
    Add a new flag to indicate .fault is tried once.  In the second try,
    skip
    ra->mmap_miss decreasing.  The filemap_fault state machine is ok with
    it.
    
    I only tested x86, didn't test other archs, but looks the change for
    other
    archs is obvious, but who knows :)
    
    < snip >
    
    Yup, arm64 needs this too! Random read improves by 250%, sequential
    read improves by 40%, and random write by 400% to an eMMC device with
    dm crypto wrapped around it.
    
    Signed-off-by: Mark Salyzyn <salyzyn@android.com>
    Signed-off-by: Riley Andrews <riandrews@android.com>
    Signed-off-by: zachariasmaladroit <zacharias.maladroit@gmail.com>
    Cc: Shaohua Li <shli@kernel.org>
    Cc: Shaohua Li <shaohua.li@fusionio.com>
    Cc: Rik van Riel <riel@redhat.com>
    Cc: Wu Fengguang <fengguang.wu@intel.com>
    ---
     arch/arm64/mm/fault.c | 1 +
     1 file changed, 1 insertion(+)

--- arch/arm64/mm/fault.c
+++ arch/arm64/mm/fault.c
@@ -301,7 +301,7 @@ retry:
 			 * Clear FAULT_FLAG_ALLOW_RETRY to avoid any risk of
 			 * starvation.
 			 */
-			mm_flags &= ~FAULT_FLAG_ALLOW_RETRY;
+			mm_flags |= FAULT_FLAG_TRIED;
 			goto retry;
 		}
 	}
