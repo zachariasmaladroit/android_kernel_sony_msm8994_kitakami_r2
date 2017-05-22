d5aff597e2fd8d681ce1d7e737f14584fe04c4e2
zachariasmaladroit <zacharias.maladroit@gmail.com>
2017-05-22 22:53:39 +0200

    Revert "futexes: Increase hash table size for better performance"
    This reverts commit db6676e544c393035aa24599950b6d76311f3777.

--- kernel/futex.c
+++ kernel/futex.c
@@ -64,7 +64,6 @@
 #include <linux/freezer.h>
 #include <linux/hugetlb.h>
 #include <linux/freezer.h>
-#include <linux/bootmem.h>
 
 #include <asm/futex.h>
 
@@ -175,6 +174,8 @@
 int __read_mostly futex_cmpxchg_enabled;
 #endif
 
+#define FUTEX_HASHBITS (CONFIG_BASE_SMALL ? 4 : 8)
+
 /*
  * Futex flags used to encode options to functions and preserve them across
  * restarts.
@@ -252,11 +253,9 @@ static const struct futex_q futex_q_init = {
 struct futex_hash_bucket {
 	spinlock_t lock;
 	struct plist_head chain;
-} ____cacheline_aligned_in_smp;
+};
 
-static unsigned long __read_mostly futex_hashsize;
-
-static struct futex_hash_bucket *futex_queues;
+static struct futex_hash_bucket futex_queues[1<<FUTEX_HASHBITS];
 
 static inline void futex_get_mm(union futex_key *key)
 {
@@ -296,7 +295,7 @@ static struct futex_hash_bucket *hash_futex(union futex_key *key)
 	u32 hash = jhash2((u32*)&key->both.word,
 			  (sizeof(key->both.word)+sizeof(key->both.ptr))/4,
 			  key->both.offset);
-	return &futex_queues[hash & (futex_hashsize - 1)];
+	return &futex_queues[hash & ((1 << FUTEX_HASHBITS)-1)];
 }
 
 /*
@@ -3052,22 +3051,11 @@ static void __init futex_detect_cmpxchg(void)
 
 static int __init futex_init(void)
 {
-	unsigned long i;
-
-#if CONFIG_BASE_SMALL
-	futex_hashsize = 16;
-#else
-	futex_hashsize = roundup_pow_of_two(256 * num_possible_cpus());
-#endif
-
-	futex_queues = alloc_large_system_hash("futex", sizeof(*futex_queues),
-					       futex_hashsize, 0,
-					       futex_hashsize < 256 ? HASH_SMALL : 0,
-					       NULL, NULL, futex_hashsize, futex_hashsize);
+	int i;
 
 	futex_detect_cmpxchg();
 
-	for (i = 0; i < futex_hashsize; i++) {
+	for (i = 0; i < ARRAY_SIZE(futex_queues); i++) {
 		plist_head_init(&futex_queues[i].chain);
 		spin_lock_init(&futex_queues[i].lock);
 	}
