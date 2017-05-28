6dd2d759f64cf45ccd197145c8b268356f84f93d
zachariasmaladroit <zacharias.maladroit@gmail.com>
2017-05-28 22:33:33 +0200

    Revert "sched: add missing __cpuinit to  sched_cpu_active"
    This reverts commit a1f797a0027d82f1bf70ca0dceffc642a4dc6b26.

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -5489,10 +5489,13 @@ static void __cpuinit set_cpu_rq_start_time(void)
 	rq->age_stamp = sched_clock_cpu(cpu);
 }
 
-static int __cpuinit sched_cpu_active(struct notifier_block *nfb,
+static int sched_cpu_active(struct notifier_block *nfb,
 				      unsigned long action, void *hcpu)
 {
 	switch (action & ~CPU_TASKS_FROZEN) {
+	case CPU_STARTING:
+		set_cpu_rq_start_time();
+		return NOTIFY_OK;
 	case CPU_DOWN_FAILED:
 		set_cpu_active((long)hcpu, true);
 		return NOTIFY_OK;
@@ -5500,7 +5503,6 @@ static int __cpuinit sched_cpu_active(struct notifier_block *nfb,
 		return NOTIFY_DONE;
 	}
 }
-
 
 static int sched_cpu_inactive(struct notifier_block *nfb,
 					unsigned long action, void *hcpu)
