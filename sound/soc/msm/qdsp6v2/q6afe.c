eceb043de3db4a92b3a52cba195252fb3e5386cd
zachariasmaladroit <zacharias.maladroit@gmail.com>
2017-07-15 20:30:47 +0200

    Revert "ASoC: qdsp6v2: fix potential bug of infinite loop"
    This reverts commit 53a6a1411ef070a7cf743bfd1ec26d75492c4af2.
    
    Conflicts:
    	sound/soc/msm/qdsp6v2/q6afe.c
    
    due to '[PORT] qdsp6v2: blacklist %p kptr_restrict'

--- sound/soc/msm/qdsp6v2/q6afe.c
+++ sound/soc/msm/qdsp6v2/q6afe.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2012-2015, 2017, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2012-2015, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -2529,9 +2529,8 @@ int q6afe_audio_client_buf_alloc_contiguous(unsigned int dir,
 	struct afe_audio_buffer *buf;
 	size_t len;
 
-	if (!(ac) || !(bufsz) || ((dir != IN) && (dir != OUT))) {
-		pr_err("%s: ac %pK bufsz %d dir %d\n", __func__, ac, bufsz,
-			dir);
+	if (!(ac) || ((dir != IN) && (dir != OUT))) {
+		pr_err("%s: ac %p dir %d\n", __func__, ac, dir);
 		return -EINVAL;
 	}
 
