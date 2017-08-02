a1ec4f465615bbd1e397183f4e3dbb3f95e6fc94
zachariasmaladroit <zacharias.maladroit@gmail.com>
2017-08-02 19:04:07 +0200

    Revert "once: make helper generic for calling functions once"
    This reverts commit f9cc971430283b76876f488bfd76a38ba97b1654.

--- lib/once.c
+++ lib/once.c
@@ -3,60 +3,52 @@
 #include <linux/once.h>
 #include <linux/random.h>
 
-struct once_work {
+struct __random_once_work {
 	struct work_struct work;
 	struct static_key *key;
 };
 
-static void once_deferred(struct work_struct *w)
+static void __random_once_deferred(struct work_struct *w)
 {
-	struct once_work *work;
+	struct __random_once_work *work;
 
-	work = container_of(w, struct once_work, work);
+	work = container_of(w, struct __random_once_work, work);
 	BUG_ON(!static_key_enabled(work->key));
 	static_key_slow_dec(work->key);
 	kfree(work);
 }
 
-static void once_disable_jump(struct static_key *key)
+static void __random_once_disable_jump(struct static_key *key)
 {
-	struct once_work *w;
+	struct __random_once_work *w;
 
 	w = kmalloc(sizeof(*w), GFP_ATOMIC);
 	if (!w)
 		return;
 
-	INIT_WORK(&w->work, once_deferred);
+	INIT_WORK(&w->work, __random_once_deferred);
 	w->key = key;
 	schedule_work(&w->work);
 }
 
-static DEFINE_SPINLOCK(once_lock);
-
-bool __do_once_start(bool *done, unsigned long *flags)
-	__acquires(once_lock)
+bool __get_random_once(void *buf, int nbytes, bool *done,
+		       struct static_key *once_key)
 {
-	spin_lock_irqsave(&once_lock, *flags);
+	static DEFINE_SPINLOCK(lock);
+	unsigned long flags;
+
+	spin_lock_irqsave(&lock, flags);
 	if (*done) {
-		spin_unlock_irqrestore(&once_lock, *flags);
-		/* Keep sparse happy by restoring an even lock count on
-		 * this lock. In case we return here, we don't call into
-		 * __do_once_done but return early in the DO_ONCE() macro.
-		 */
-		__acquire(once_lock);
+		spin_unlock_irqrestore(&lock, flags);
 		return false;
 	}
 
+	get_random_bytes(buf, nbytes);
+	*done = true;
+	spin_unlock_irqrestore(&lock, flags);
+
+	__random_once_disable_jump(once_key);
+
 	return true;
 }
-EXPORT_SYMBOL(__do_once_start);
-
-void __do_once_done(bool *done, struct static_key *once_key,
-		    unsigned long *flags)
-	__releases(once_lock)
-{
-	*done = true;
-	spin_unlock_irqrestore(&once_lock, *flags);
-	once_disable_jump(once_key);
-}
-EXPORT_SYMBOL(__do_once_done);
+EXPORT_SYMBOL(__get_random_once);
