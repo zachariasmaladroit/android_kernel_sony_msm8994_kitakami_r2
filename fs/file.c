3d480759f5b5f95703bf079ea3861b5325bf68e7
zachariasmaladroit <zacharias.maladroit@gmail.com>
2017-08-06 14:07:45 +0200

    Revert "vfs: conditionally clear close-on-exec flag"
    This reverts commit e50ef8ff4eaf229030bc588b6a043a0fd840b013.

--- fs/file.c
+++ fs/file.c
@@ -238,8 +238,7 @@ static inline void __set_close_on_exec(int fd, struct fdtable *fdt)
 
 static inline void __clear_close_on_exec(int fd, struct fdtable *fdt)
 {
-	if (test_bit(fd, fdt->close_on_exec))
-		__clear_bit(fd, fdt->close_on_exec);
+	__clear_bit(fd, fdt->close_on_exec);
 }
 
 static inline void __set_open_fd(unsigned int fd, struct fdtable *fdt)
