b036871e003622dc3e5f453c986f00763c6e5510
zachariasmaladroit <zacharias.maladroit@gmail.com>
2017-09-09 03:09:56 +0200

    Revert "sched/debug: Don't dump sched debug info in SysRq-W"
    This reverts commit b781f8386d71993a61f24a2e4553c55224fb4709.

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -6947,8 +6947,7 @@ void show_state_filter(unsigned long state_filter)
 	touch_all_softlockup_watchdogs();
 
 #ifdef CONFIG_SYSRQ_SCHED_DEBUG
-	if (!state_filter)
-		sysrq_sched_debug_show();
+	sysrq_sched_debug_show();
 #endif
 	rcu_read_unlock();
 	/*
