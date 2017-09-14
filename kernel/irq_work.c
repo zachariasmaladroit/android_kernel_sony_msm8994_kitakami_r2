cf743d3c17e8c2beaa0554b637aacaab2042a607
zachariasmaladroit <zacharias.maladroit@gmail.com>
2017-09-14 17:49:14 +0200

    Revert "irq_work: Don't reinvent the wheel but use existing llist API"
    This reverts commit 04c8d0ee2efeccbca455b6886f5eb91dfd60f8fd.

--- kernel/irq_work.c
+++ kernel/irq_work.c
@@ -135,7 +135,11 @@ static void irq_work_run_list(struct llist_head *list)
 		return;
 
 	llnode = llist_del_all(list);
-	llist_for_each_entry(work, llnode, llnode) {
+	while (llnode != NULL) {
+		work = llist_entry(llnode, struct irq_work, llnode);
+
+		llnode = llist_next(llnode);
+
 		/*
 		 * Clear the PENDING bit, after this point the @work
 		 * can be re-used.
