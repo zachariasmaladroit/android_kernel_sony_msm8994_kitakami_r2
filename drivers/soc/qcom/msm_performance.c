d5c87924365095cb7f3c32ac21edc7042f9ee2fb
zachariasmaladroit <zacharias.maladroit@gmail.com>
2017-09-18 18:20:55 +0200

    Revert "msm_performance: remove unused variable"
    This reverts commit 58ef4863e128f84283521ed1763a1ebc56c897ae.

--- drivers/soc/qcom/msm_performance.c
+++ drivers/soc/qcom/msm_performance.c
@@ -408,6 +408,7 @@ static int set_cpu_min_freq(const char *buf, const struct kernel_param *kp)
 	struct cpufreq_policy policy;
 	cpumask_var_t limit_mask;
 	int ret;
+	const char *reset = "0:0 4:0";
 
 	int msm_perf = strcmp(current->comm, "perfd");
 
