e3dd14bfd6db74791df3b91e404aa4be6bc8e960
zachariasmaladroit <zacharias.maladroit@gmail.com>
2017-09-22 01:06:39 +0200

    Revert "[PORT] kernel: Convert a few critical drivers to use jiffies"
    This reverts commit 4785641ef81f8076b6d3b37745fcd434fa641f17.

--- drivers/soc/qcom/glink_smd_xprt.c
+++ drivers/soc/qcom/glink_smd_xprt.c
@@ -1268,7 +1268,7 @@ static int ssr(struct glink_transport_if *if_ptr)
 
 	einfo->xprt_if.glink_core_if_ptr->link_down(&einfo->xprt_if);
 	queue_delayed_work(system_power_efficient_wq,
-				&einfo->ssr_work, msecs_to_jiffies(5000));
+				&einfo->ssr_work, 5 * HZ);
 	return 0;
 }
 
