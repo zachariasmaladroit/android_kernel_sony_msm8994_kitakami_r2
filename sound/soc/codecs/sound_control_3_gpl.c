d8ecbdfba40556629acf784ebdc7c6bd1dff8db4
zachariasmaladroit <zacharias.maladroit@gmail.com>
2017-09-22 00:30:52 +0200

    Revert "ASoC: wcd9330: add PDesireAudio toggle for faux sound"
    This reverts commit c8698af62e7e1f25fe19aa4c3244394a597a4d03.

--- sound/soc/codecs/sound_control_3_gpl.c
+++ sound/soc/codecs/sound_control_3_gpl.c
@@ -32,8 +32,6 @@
 extern struct snd_soc_codec *fauxsound_codec_ptr;
 extern int wcd9xxx_hw_revision;
 
-extern int PDesireAudio;
-
 static int snd_ctrl_locked = 2;
 static int snd_rec_ctrl_locked = 0;
 
@@ -295,22 +293,6 @@ static ssize_t headphone_gain_store(struct kobject *kobj,
 	return count;
 }
 
-static ssize_t pdesireaudio_show(struct kobject *kobj,
-                struct kobj_attribute *attr, char *buf)
-{
-        return sprintf(buf, "%d\n", PDesireAudio);
-}
-
-static ssize_t pdesireaudio_store(struct kobject *kobj,
-                 struct kobj_attribute *attr, const char *buf, size_t count)
-{
-        if (buf[0] >= '0' && buf[0] <= '1' && buf[1] == '\n')
-                if (PDesireAudio != buf[0] - '0')
-                        PDesireAudio = buf[0] - '0';
-
-        return count;
-}
-
 static unsigned int selected_reg = 0xdeadbeef;
 
 static ssize_t sound_reg_select_store(struct kobject *kobj,
@@ -386,19 +368,12 @@ static struct kobj_attribute headphone_gain_attribute =
 		headphone_gain_show,
 		headphone_gain_store);
 
-static struct kobj_attribute PDesireAudio_mode_attribute =
-        __ATTR(PDesireAudio_enabled,
-                0666,
-                pdesireaudio_show,
-                pdesireaudio_store);
-
 static struct attribute *sound_control_attrs[] =
 	{
 		&cam_mic_gain_attribute.attr,
 		&mic_gain_attribute.attr,
 		&speaker_gain_attribute.attr,
 		&headphone_gain_attribute.attr,
-                &PDesireAudio_mode_attribute.attr,
 		&sound_reg_sel_attribute.attr,
 		&sound_reg_read_attribute.attr,
 		&sound_reg_write_attribute.attr,
