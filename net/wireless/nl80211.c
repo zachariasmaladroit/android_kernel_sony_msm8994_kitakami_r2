1f7918ff1f1e42c1fe721d102828ba4de8139e3a
zachariasmaladroit <zacharias.maladroit@gmail.com>
2017-10-05 15:25:38 +0200

    net/wireless/nl80211.c: fix build failure without NL80211_TESTMODE set
    Signed-off-by: zachariasmaladroit <zacharias.maladroit@gmail.com>

--- net/wireless/nl80211.c
+++ net/wireless/nl80211.c
@@ -6865,9 +6865,11 @@ void __cfg80211_send_event_skb(struct sk_buff *skb, gfp_t gfp)
 	if (data->nla_type == NL80211_ATTR_VENDOR_DATA)
 		genlmsg_multicast_netns(wiphy_net(&rdev->wiphy), skb, 0,
 			nl80211_vendor_mcgrp.id, gfp);
+#ifdef CONFIG_NL80211_TESTMODE
 	else
 		genlmsg_multicast_netns(wiphy_net(&rdev->wiphy), skb, 0,
 			nl80211_testmode_mcgrp.id, gfp);
+#endif
 }
 EXPORT_SYMBOL(__cfg80211_send_event_skb);
 
