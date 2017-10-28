f7ea9e3b519ffaf85448c5abf38d3a24b1b5acd3
Pavankumar Kondeti <pkondeti@codeaurora.org>
2017-10-28 23:11:53 +0200

    [BACKPORT] softirq: defer softirq processing to ksoftirqd if CPU is busy with RT
    Defer the softirq processing to ksoftirqd if a RT task is running
    or queued on the current CPU. This complements the RT task placement
    algorithm which tries to find a CPU that is not currently busy with
    softirqs.
    
    Currently NET_TX, NET_RX, BLOCK and TASKLET softirqs are only deferred
    as they can potentially run for long time.
    
    Change-Id: Id7665244af6bbd5a96d9e591cf26154e9eaa860c
    Signed-off-by: Pavankumar Kondeti <pkondeti@codeaurora.org>
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>
    Signed-off-by: zachariasmaladroit <zacharias.maladroit@gmail.com>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1928,6 +1928,7 @@ extern int set_cpus_allowed_ptr(struct task_struct *p,
 				const struct cpumask *new_mask);
 extern void sched_set_cpu_cstate(int cpu, int cstate,
 			 int wakeup_energy, int wakeup_latency);
+extern bool cpupri_check_rt(void);
 #else
 static inline void do_set_cpus_allowed(struct task_struct *p,
 				      const struct cpumask *new_mask)
@@ -1943,6 +1944,10 @@ static inline int set_cpus_allowed_ptr(struct task_struct *p,
 static inline void
 sched_set_cpu_cstate(int cpu, int cstate, int wakeup_energy, int wakeup_latency)
 {
+}
+static inline bool cpupri_check_rt(void)
+{
+	return false;
 }
 #endif
 
