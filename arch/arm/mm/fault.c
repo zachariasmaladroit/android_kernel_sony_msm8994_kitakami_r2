847f001dbee317db1c918760d41a654ace973e74
Peter Zijlstra <a.p.zijlstra@chello.nl>
2017-11-08 23:32:51 +0100

    [PORT] mm: pagefault_disabled()
    Wrap the test for pagefault_disabled() into a helper, this allows us
    to remove the need for current->pagefault_disabled on !-rt kernels.
    
    Signed-off-by: Peter Zijlstra <a.p.zijlstra@chello.nl>
    Link:
    http://lkml.kernel.org/n/tip-3yy517m8zsi9fpsf14xfaqkw@git.kernel.org
    ---
     arch/alpha/mm/fault.c      |  2 +-
     arch/arm/mm/fault.c        |  2 +-
     arch/arm64/mm/fault.c        |  2 +-
     arch/avr32/mm/fault.c      |  3 +--
     arch/cris/mm/fault.c       |  2 +-
     arch/frv/mm/fault.c        |  2 +-
     arch/ia64/mm/fault.c       |  2 +-
     arch/m32r/mm/fault.c       |  2 +-
     arch/m68k/mm/fault.c       |  2 +-
     arch/microblaze/mm/fault.c |  2 +-
     arch/mips/mm/fault.c       |  2 +-
     arch/mn10300/mm/fault.c    |  2 +-
     arch/parisc/mm/fault.c     |  2 +-
     arch/powerpc/mm/fault.c    |  2 +-
     arch/s390/mm/fault.c       |  8 ++++----
     arch/score/mm/fault.c      |  2 +-
     arch/sh/mm/fault.c         |  2 +-
     arch/sparc/mm/fault_32.c   |  2 +-
     arch/sparc/mm/fault_64.c   |  2 +-
     arch/tile/mm/fault.c       |  2 +-
     arch/um/kernel/trap.c      |  2 +-
     arch/x86/mm/fault.c        |  2 +-
     arch/xtensa/mm/fault.c     |  2 +-
     include/linux/sched.h      | 14 ++++++++++++++
     kernel/fork.c              |  2 ++
    25 files changed, 42 insertions(+), 27 deletions(-)
    
    conflict in arch/arm/mm/fault.c due to 'irqs_disabled()'
    
    confict in include/linux/sched.h due to '#include <linux/cpufreq.h>'
    
    Signed-off-by: zachariasmaladroit <zacharias.maladroit@gmail.com>

--- arch/arm/mm/fault.c
+++ arch/arm/mm/fault.c
@@ -285,8 +285,7 @@ do_page_fault(unsigned long addr, unsigned int fsr, struct pt_regs *regs)
 	 * If we're in an interrupt, or have no irqs, or have no user
 	 * context, we must not take the fault..
 	 */
-	if (in_atomic() || irqs_disabled() || !mm
-		|| current->pagefault_disabled)
+	if (irqs_disabled() || !mm || current->pagefault_disabled)
 		goto no_context;
 
 	if (user_mode(regs))
