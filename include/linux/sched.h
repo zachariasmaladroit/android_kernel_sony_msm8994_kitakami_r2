8ed26ebb869e243680c984c3c759394e0e357b4c
Thomas Gleixner <tglx@linutronix.de>
2017-11-09 21:49:45 +0100

    sched-delay-put-task.patch
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    ---
     include/linux/sched.h | 13 +++++++++++++
     kernel/fork.c         | 15 ++++++++++++++-
     2 files changed, 27 insertions(+), 1 deletion(-)
    
    conflicts in fork.c due to 'task_free_unregister' , 'task_free_register'
    and CONFIG_TASK_CPUFREQ_STATS
    
    + enable by default
    
    Signed-off-by: zachariasmaladroit <zacharias.maladroit@gmail.com>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1212,6 +1212,9 @@ struct sched_rt_entity {
 	/* rq "owned" by this entity/group: */
 	struct rt_rq		*my_q;
 #endif
+//#ifdef CONFIG_PREEMPT_RT_BASE
+	struct rcu_head put_rcu;
+//#endif
 };
 
 struct sched_dl_entity {
@@ -1912,6 +1915,15 @@ extern struct pid *cad_pid;
 extern void free_task(struct task_struct *tsk);
 #define get_task_struct(tsk) do { atomic_inc(&(tsk)->usage); } while(0)
 
+//#ifdef CONFIG_PREEMPT_RT_BASE
+extern void __put_task_struct_cb(struct rcu_head *rhp);
+
+static inline void put_task_struct(struct task_struct *t)
+{
+	if (atomic_dec_and_test(&t->usage))
+		call_rcu(&t->put_rcu, __put_task_struct_cb);
+}
+/*#else
 extern void __put_task_struct(struct task_struct *t);
 
 static inline void put_task_struct(struct task_struct *t)
@@ -1919,6 +1931,7 @@ static inline void put_task_struct(struct task_struct *t)
 	if (atomic_dec_and_test(&t->usage))
 		__put_task_struct(t);
 }
+#endif*/
 
 #ifdef CONFIG_VIRT_CPU_ACCOUNTING_GEN
 extern void task_cputime(struct task_struct *t,
--- kernel/fork.c
+++ kernel/fork.c
@@ -259,6 +259,9 @@ static void cpufreq_stats_tsk_free(struct task_struct *tsk)
 }
 #endif
 
+//#ifdef CONFIG_PREEMPT_RT_BASE
+static
+//#endif
 void __put_task_struct(struct task_struct *tsk)
 {
 	WARN_ON(!tsk->exit_state);
@@ -277,7 +280,18 @@ void __put_task_struct(struct task_struct *tsk)
 	if (!profile_handoff_task(tsk))
 		free_task(tsk);
 }
+/*#ifndef CONFIG_PREEMPT_RT_BASE
 EXPORT_SYMBOL_GPL(__put_task_struct);
+#else*/
+void __put_task_struct_cb(struct rcu_head *rhp)
+{
+	struct task_struct *tsk = container_of(rhp, struct task_struct, put_rcu);
+
+	__put_task_struct(tsk);
+
+}
+EXPORT_SYMBOL_GPL(__put_task_struct_cb);
+//#endif
 
 void __init __weak arch_task_cache_init(void) { }
 
