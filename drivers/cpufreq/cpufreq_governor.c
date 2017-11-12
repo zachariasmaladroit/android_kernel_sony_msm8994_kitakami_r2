7ad54e39b7413f3f67be25a565786c3e60bc7e16
zachariasmaladroit <zacharias.maladroit@gmail.com>
2017-11-12 02:26:55 +0100

    Revert "cpufreq: conservative: Decrease frequency faster when the timer deferred"
    we DO want the governor to ramp down slowly otherwise user experience
    isn't smooth anymore ...
    
    This reverts commit d964249efc9af264e660320d2c1b665f5cbf3209.
    
    Conflicts:
    	drivers/cpufreq/cpufreq_conservative.c
    
    due to 'cpufreq: conservative: Fix condition logic in touch boost'

--- drivers/cpufreq/cpufreq_governor.c
+++ drivers/cpufreq/cpufreq_governor.c
@@ -44,7 +44,7 @@ void dbs_check_cpu(struct dbs_data *dbs_data, int cpu)
 	struct ac_dbs_tuners *ac_tuners = dbs_data->tuners;
 	struct cpufreq_policy *policy;
 	unsigned int sampling_rate;
-	unsigned int max_load = 0, deferred_periods = UINT_MAX;
+	unsigned int max_load = 0;
 	unsigned int ignore_nice;
 	unsigned int j;
 	struct cpufreq_govinfo govinfo;
@@ -150,10 +150,6 @@ void dbs_check_cpu(struct dbs_data *dbs_data, int cpu)
 		 */
 		if (unlikely(wall_time > (2 * sampling_rate))) {
 			unsigned int busy = wall_time - idle_time;
-			unsigned int periods = wall_time / sampling_rate;
-
-			if (periods < deferred_periods)
-				deferred_periods = periods;
 
 			if (busy > sampling_rate)
 				load = 100;
@@ -179,7 +175,6 @@ void dbs_check_cpu(struct dbs_data *dbs_data, int cpu)
 		if (load > max_load)
 			max_load = load;
 	}
-	cdbs->deferred_periods = deferred_periods;
 
 	dbs_data->cdata->gov_check_cpu(cpu, max_load);
 }
