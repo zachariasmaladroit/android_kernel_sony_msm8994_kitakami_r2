18daa09aa7f785cbd16f171c9163b633cde27c5d
Steven Rostedt <rostedt@goodmis.org>
2017-11-13 01:13:22 +0100

    [fixed][adapted] sched/rt: Fix wait_task_interactive() to test  rt_spin_lock state
    The wait_task_interactive() will have a task sleep waiting for another
    task to have a certain state. But it ignores the rt_spin_locks state
    and can return with an incorrect result if the task it is waiting
    for is blocked on a rt_spin_lock() and is waking up.
    
    The rt_spin_locks save the tasks state in the saved_state field
    and the wait_task_interactive() must also test that state.
    
    Signed-off-by: Steven Rostedt <rostedt@goodmis.org>
    Cc: Carsten Emde <C.Emde@osadl.org>
    Cc: John Kacur <jkacur@redhat.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Clark Williams <clark.williams@gmail.com>
    Cc: stable-rt@vger.kernel.org
    Link: http://lkml.kernel.org/r/20120301190345.979435764@goodmis.org
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    ---
     kernel/sched/core.c | 6 ++++--
     1 file changed, 4 insertions(+), 2 deletions(-)
    
    conflict due to 'cpu_relaxed_read_long' change
    
    Signed-off-by: zachariasmaladroit <zacharias.maladroit@gmail.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -3368,7 +3368,8 @@ unsigned long wait_task_inactive(struct task_struct *p, long match_state)
 		 */
 		while (task_running(rq, p)) {
 			if (match_state && unlikely(cpu_relaxed_read_long
-				(&(p->state)) != match_state))
+				(&(p->state)) && unlikely(cpu_relaxed_read_long
+				(&(p->saved_state)) != match_state)))
 				return 0;
 			cpu_read_relax();
 		}
@@ -3383,7 +3384,8 @@ unsigned long wait_task_inactive(struct task_struct *p, long match_state)
 		running = task_running(rq, p);
 		on_rq = p->on_rq;
 		ncsw = 0;
-		if (!match_state || p->state == match_state)
+		if (!match_state || p->state == match_state
+		    || p->saved_state == match_state)
 			ncsw = p->nvcsw | LONG_MIN; /* sets MSB */
 		task_rq_unlock(rq, p, &flags);
 
