1974a77dc90bf13ccf4971f627a9e6eb31ecc845
zachariasmaladroit <zacharias.maladroit@gmail.com>
2017-11-15 04:03:36 +0100

    clearpad_core: convert the poll_work jobs using powerefficient workqueues
    Signed-off-by: zachariasmaladroit <zacharias.maladroit@gmail.com>

--- drivers/input/touchscreen/clearpad_core.c
+++ drivers/input/touchscreen/clearpad_core.c
@@ -2073,7 +2073,8 @@ static void clearpad_wd_status_poll(struct work_struct *work)
 		} else {
 			this->reset_count = 0;
 		}
-		schedule_delayed_work(&this->wd_poll_work, this->wd_poll_t_jf);
+		queue_delayed_work(system_power_efficient_wq,
+				&this->wd_poll_work, this->wd_poll_t_jf);
 	}
 	UNLOCK(this);
 }
@@ -2087,7 +2088,8 @@ static void clearpad_wd_update(struct clearpad_t *this, bool sync)
 
 	LOCK(this);
 	if ((this->active & SYN_ACTIVE_POWER) && !this->flash_requested)
-		schedule_delayed_work(&this->wd_poll_work, this->wd_poll_t_jf);
+		queue_delayed_work(system_power_efficient_wq,
+				&this->wd_poll_work, this->wd_poll_t_jf);
 	UNLOCK(this);
 }
 
