e4f53f245b2e20b73ee8ce111fe8c4f7dbeaa5d5
zachariasmaladroit <zacharias.maladroit@gmail.com>
2018-02-27 16:40:28 +0100

    Revert [scheduler ioprio changes missing] "gpu: adreno: run input_work with highpriority WQ after slumber"
    This reverts commit 8fc5e248b04e6eb772877ed304d50ec2a96bf68c.

--- drivers/gpu/msm/adreno.c
+++ drivers/gpu/msm/adreno.c
@@ -271,8 +271,7 @@ static void adreno_input_event(struct input_handle *handle, unsigned int type,
 		mod_timer(&device->idle_timer,
 			jiffies + device->pwrctrl.interval_timeout);
 	} else if (device->state == KGSL_STATE_SLUMBER) {
-		queue_work(system_highpri_wq,
-				&adreno_dev->input_work);
+		schedule_work(&adreno_dev->input_work);
 	}
 }
 
