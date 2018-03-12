c79b833f2ac68ea02b3dd4517a862d604dd2742b
zachariasmaladroit <zacharias.maladroit@gmail.com>
2018-03-12 01:42:53 +0100

    Revert "[BACKPORT] softirq: defer softirq processing to ksoftirqd if CPU is busy"
    This reverts commit f7ea9e3b519ffaf85448c5abf38d3a24b1b5acd3.

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1928,7 +1928,6 @@ extern int set_cpus_allowed_ptr(struct task_struct *p,
 				const struct cpumask *new_mask);
 extern void sched_set_cpu_cstate(int cpu, int cstate,
 			 int wakeup_energy, int wakeup_latency);
-extern bool cpupri_check_rt(void);
 #else
 static inline void do_set_cpus_allowed(struct task_struct *p,
 				      const struct cpumask *new_mask)
@@ -1944,10 +1943,6 @@ static inline int set_cpus_allowed_ptr(struct task_struct *p,
 static inline void
 sched_set_cpu_cstate(int cpu, int cstate, int wakeup_energy, int wakeup_latency)
 {
-}
-static inline bool cpupri_check_rt(void)
-{
-	return false;
 }
 #endif
 
