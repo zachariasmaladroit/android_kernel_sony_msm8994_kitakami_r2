213a9a9de1f6bcdeeaebd0ee108626be32453a3e
zachariasmaladroit <zacharias.maladroit@gmail.com>
2018-04-21 23:35:04 +0200

    Revert "msm: kgsl: Add array of page pointers to memdesc"
    This reverts commit 5e33d71e25aca702acee713760b61c3356ba9fd0.
    
    Conflicts:
    	drivers/gpu/msm/kgsl.h
    
    keep newer copyright

--- drivers/gpu/msm/kgsl.h
+++ drivers/gpu/msm/kgsl.h
@@ -152,8 +152,6 @@ struct kgsl_memdesc {
 	unsigned int flags; /* Flags set from userspace */
 	struct device *dev;
 	struct dma_attrs attrs;
-	struct page **pages;
-	unsigned int page_count;
 };
 
 /*
