02acc13e3373787a436e1f193c7bad28150d3e3c
zachariasmaladroit <zacharias.maladroit@gmail.com>
2018-04-21 17:43:42 +0200

    Revert "[BACKPORT] mm/vmpressure.c: fix race in vmpressure_work_fn() + mutex to"
    This reverts commit 26682262978dcc13ca68cf7f8e518289b2f47b9e.

--- mm/vmpressure.c
+++ mm/vmpressure.c
@@ -227,7 +227,6 @@ static void vmpressure_work_fn(struct work_struct *work)
 	unsigned long scanned;
 	unsigned long reclaimed;
 
-	spin_lock(&vmpr->sr_lock);
 	/*
 	 * Several contexts might be calling vmpressure(), so it is
 	 * possible that the work was rescheduled again before the old
@@ -236,16 +235,15 @@ static void vmpressure_work_fn(struct work_struct *work)
 	 * here. No need for any locks here since we don't care if
 	 * vmpr->reclaimed is in sync.
 	 */
-	scanned = vmpr->scanned;
-	if (!scanned) {
-		spin_unlock(&vmpr->sr_lock);
+	if (!vmpr->scanned)
 		return;
-	}
 
+	mutex_lock(&vmpr->sr_lock);
+	scanned = vmpr->scanned;
 	reclaimed = vmpr->reclaimed;
 	vmpr->scanned = 0;
 	vmpr->reclaimed = 0;
-	spin_unlock(&vmpr->sr_lock);
+	mutex_unlock(&vmpr->sr_lock);
 
 	do {
 		if (vmpressure_event(vmpr, scanned, reclaimed))
@@ -289,11 +287,11 @@ void vmpressure_memcg(gfp_t gfp, struct mem_cgroup *memcg,
 	if (!scanned)
 		return;
 
-	spin_lock(&vmpr->sr_lock);
+	mutex_lock(&vmpr->sr_lock);
 	vmpr->scanned += scanned;
 	vmpr->reclaimed += reclaimed;
 	scanned = vmpr->scanned;
-	spin_unlock(&vmpr->sr_lock);
+	mutex_unlock(&vmpr->sr_lock);
 
 	if (scanned < vmpressure_win || work_pending(&vmpr->work))
 		return;
@@ -313,7 +311,7 @@ void vmpressure_global(gfp_t gfp, unsigned long scanned,
 	if (!scanned)
 		return;
 
-	spin_lock(&vmpr->sr_lock);
+	mutex_lock(&vmpr->sr_lock);
 	vmpr->scanned += scanned;
 	vmpr->reclaimed += reclaimed;
 
@@ -323,16 +321,16 @@ void vmpressure_global(gfp_t gfp, unsigned long scanned,
 	stall = vmpr->stall;
 	scanned = vmpr->scanned;
 	reclaimed = vmpr->reclaimed;
-	spin_unlock(&vmpr->sr_lock);
+	mutex_unlock(&vmpr->sr_lock);
 
 	if (scanned < vmpressure_win)
 		return;
 
-	spin_lock(&vmpr->sr_lock);
+	mutex_lock(&vmpr->sr_lock);
 	vmpr->scanned = 0;
 	vmpr->reclaimed = 0;
 	vmpr->stall = 0;
-	spin_unlock(&vmpr->sr_lock);
+	mutex_unlock(&vmpr->sr_lock);
 
 	pressure = vmpressure_calc_pressure(scanned, reclaimed);
 	pressure = vmpressure_account_stall(pressure, stall, scanned);
@@ -499,7 +497,7 @@ void vmpressure_cleanup(struct vmpressure *vmpr)
  */
 void vmpressure_init(struct vmpressure *vmpr)
 {
-	spin_lock_init(&vmpr->sr_lock);
+	mutex_init(&vmpr->sr_lock);
 	mutex_init(&vmpr->events_lock);
 	INIT_LIST_HEAD(&vmpr->events);
 	INIT_WORK(&vmpr->work, vmpressure_work_fn);
