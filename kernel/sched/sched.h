5c72672ce10e93b66307e1966f957d1900964718
zachariasmaladroit <zacharias.maladroit@gmail.com>
2018-04-29 18:02:35 +0200

    Revert "sched, nohz: Change rq->nr_running to always use wrappers"
    This reverts commit e3cd5f8769301f55636f256c08536a8409ed73fc.

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -1454,14 +1454,12 @@ static inline u64 steal_ticks(u64 steal)
 }
 #endif
 
-static inline void add_nr_running(struct rq *rq, unsigned count)
+static inline void inc_nr_running(struct rq *rq)
 {
-	unsigned prev_nr = rq->nr_running;
-	sched_update_nr_prod(cpu_of(rq), count, true);
+	sched_update_nr_prod(cpu_of(rq), 1, true);
+	rq->nr_running++;
 
-	rq->nr_running = prev_nr + count;
-
-	if (prev_nr < 2 && rq->nr_running >= 2) {
+	if (rq->nr_running == 2) {
 #ifdef CONFIG_SMP
 		if (!rq->rd->overload)
 			rq->rd->overload = true;
@@ -1477,10 +1475,10 @@ static inline void add_nr_running(struct rq *rq, unsigned count)
 	}
 }
 
-static inline void sub_nr_running(struct rq *rq, unsigned count)
+static inline void dec_nr_running(struct rq *rq)
 {
-	sched_update_nr_prod(cpu_of(rq), count, false);
-	rq->nr_running -= count;
+	sched_update_nr_prod(cpu_of(rq), 1, false);
+	rq->nr_running--;
 }
 
 static inline void rq_last_tick_reset(struct rq *rq)
